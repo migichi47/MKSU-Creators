1952157d63a450c0b0d41fd4a1500199021b4f4e
Peter Migichi <petermigichi47@gmail.com>
2026-06-19 10:57:29 +0300

    Generated tiles html

---
+++ main.js
@@ -0,0 +1,199 @@
+creators = {
+  dancers: [
+    {
+      image: "images/morin.jpg",
+      name: 'Morin',
+      followers: 34000,
+      year: 4,
+    },
+    {
+      image: "images/mukeli.jpg",
+      name: 'Mukelih',
+      followers: 4400,
+      year: 1,
+    },
+    {
+      image: "images/cute-adorable-little-girl-character-avatar-isolated_925324-1724.avif",
+      name: 'Mourine',
+      followers: 3400,
+      year: 1,
+    },
+    {
+      image: "images/mwende.jpg",
+      name: 'Mwende',
+      followers: 1100,
+      year: 3,
+    }
+  ],
+
+  vloggers: [
+    {
+      image: "images/WhatsApp Image 2026-04-21 at 2.53.05 PM (1).jpeg",
+      name: 'Migichi',
+      followers: 3400,
+      year: 1,
+    },
+    {
+      image: "images/tabi mwika.webp",
+      name: 'Tabi Mwikaa',
+      followers: 2400,
+      year: 1,
+    },
+    {
+      image: "images/blessing.jpg",
+      name: 'Mourine',
+      followers: 1700,
+      year: 1,
+    },
+    {
+      image: "images/Marrion.jpg",
+      name: 'Marrion',
+      followers: 6800,
+    },
+    {
+      image: '',
+      name: 'Mksu Vibes',
+      followers: 3400,
+      year: false,
+    }
+  ],
+
+  comedians: [
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    }
+  ],
+
+  influencers: [
+    {
+      image: "images/gym reaper.jpg",
+      name: 'Gym Reaper',
+      followers: "2300",
+      year: "",
+    },
+    {
+      image: "images/_opiri.jpg",
+      name: '_opiri',
+      followers: "3300",
+      year: "",
+    },
+    {
+      image: "images/Daisy.webp",
+      name: 'Daisy',
+      followers: "5900",
+      year: "",
+    },
+    {
+      image: "images/Miss Throne.jpg",
+      name: 'Miss Throne',
+      followers: "3400",
+      year: "",
+    },
+    {
+      image: "images/pngtree-cartoon-hand-drawn-handsome-boy-avatar-png-image_16798195.webp",
+      name: 'Benzema',
+      followers: "11400",
+      year: "",
+    }
+  ],
+
+  musicians: [
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    }
+  ],
+
+  
+};
+
+
+//generating HTML
+const generateHtml  = (creatorsList) => {
+  let html = '';
+
+  creatorsList.forEach(creator => { 
+    html += `
+      <div class="preview" >
+        <img class="cover-photo" src="${creator.image || "images/default.png"}" alt="">
+        <div class="preview-text">
+          <div>
+            <p class="name" >${creator.name}</p>
+          </div>
+          <div class="description">
+            <p style="display: inline-block;" >${creator.followers / 1000}k followers</p>&#183
+            <p style="display: inline-block;" >Year ${creator.year || 'N/A'}</p>
+          </div>
+        </div>
+      </div>
+    `
+  })
+
+  return html;
+}
+
+document.querySelector(".js-dancers").innerHTML = generateHtml(creators.dancers);
+document.querySelector(".js-vloggers").innerHTML = generateHtml(creators.vloggers);
+document.querySelector(".js-comedians").innerHTML = generateHtml(creators.comedians);
+document.querySelector(".js-influencers").innerHTML = generateHtml(creators.influencers);
+document.querySelector(".js-musicians").innerHTML = generateHtml(creators.musicians);
+
+
+document.querySelectorAll('.preview').forEach(preview => {
+  preview.addEventListener('click', () => {
+    console.log('Voted!')
+  })
+});
