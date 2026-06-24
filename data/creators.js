28b239bbc0cdb9d5f2351d1691293ff32fcc6f85
Peter Migichi <petermigichi47@gmail.com>
2026-06-24 23:30:35 +0300

    used modules to import creators data to main.js

---
+++ data/creators.js
@@ -0,0 +1,159 @@
+export const creators = {
+  dancers: [
+    {
+      image: "images/morin.jpg",
+      name: 'Morin',
+      followers: 34000,
+      year: 4,
+    },
+    {
+      image: "images/mukeli.jpg",
+      name: 'Mukelih',
+      followers: 4400,
+      year: 1,
+    },
+    {
+      image: "images/cute-adorable-little-girl-character-avatar-isolated_925324-1724.avif",
+      name: 'Mourine',
+      followers: 3400,
+      year: 1,
+    },
+    {
+      image: "images/mwende.jpg",
+      name: 'Mwende',
+      followers: 1100,
+      year: 3,
+    }
+  ],
+
+  vloggers: [
+    {
+      image: "images/WhatsApp Image 2026-04-21 at 2.53.05 PM (1).jpeg",
+      name: 'Migichi',
+      followers: 3400,
+      year: 1,
+    },
+    {
+      image: "images/tabi mwika.webp",
+      name: 'Tabi Mwikaa',
+      followers: 2400,
+      year: 1,
+    },
+    {
+      image: "images/blessing.jpg",
+      name: 'Mourine',
+      followers: 1700,
+      year: 1,
+    },
+    {
+      image: "images/Marrion.jpg",
+      name: 'Marrion',
+      followers: 6800,
+    },
+    {
+      image: '',
+      name: 'Mksu Vibes',
+      followers: 3400,
+      year: false,
+    }
+  ],
+
+  comedians: [
+    {
+      image: "",
+      name: 'Innocent',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: 'Harrison',
+      followers: "100000",
+      year: "7",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    }
+  ],
+
+  influencers: [
+    {
+      image: "images/gym reaper.jpg",
+      name: 'Gym Reaper',
+      followers: "2300",
+      year: "",
+    },
+    {
+      image: "images/_opiri.jpg",
+      name: '_opiri',
+      followers: "3300",
+      year: "",
+    },
+    {
+      image: "images/Daisy.webp",
+      name: 'Daisy',
+      followers: "5900",
+      year: "",
+    },
+    {
+      image: "images/Miss Throne.jpg",
+      name: 'Miss Throne',
+      followers: "3400",
+      year: "",
+    },
+    {
+      image: "images/pngtree-cartoon-hand-drawn-handsome-boy-avatar-png-image_16798195.webp",
+      name: 'Benzema',
+      followers: "11400",
+      year: "",
+    }
+  ],
+
+  musicians: [
+    {
+      image: "",
+      name: 'Bami Nui',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    },
+    {
+      image: "",
+      name: '',
+      followers: "",
+      year: "",
+    }
+  ],
+};
