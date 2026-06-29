5038c9ff0da5b183fbde2e80461df2b900a58d21
Peter Migichi <petermigichi47@gmail.com>
2026-06-29 11:20:28 +0300

    added function to about buttons

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -2,3 +2,8 @@ document.querySelector('.js-start-voting-btn')
 .addEventListener('click', () => {
   window.location.href = '../main.html'
 })
+
+document.querySelector('.js-learn-more-btn')
+.addEventListener('click', () => {
+  window.location.href = '#about'
+})
