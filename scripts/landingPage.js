fd206d513654a6b6d36229dd003e981af3e82de8
Peter Migichi <petermigichi47@gmail.com>
2026-06-30 07:36:23 +0300

    Created the category cards at the bottom of the landing page

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -2,3 +2,8 @@ document.querySelector('.js-vote-now-btn')
 .addEventListener('click', () => {
   window.location.href = '../main.html'
 })
+
+document.querySelectorAll('.category-card')
+.addEventListener('click', () => {
+  window.location.href = '#top';
+})
