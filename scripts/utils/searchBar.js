c6cf86e4171a63ce486f4a07b318ee156bae844b
Peter Migichi <petermigichi47@gmail.com>
2026-06-30 10:15:37 +0300

    added flick on search function, to enhance visuals

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -3,27 +3,36 @@ const notyf = new Notyf();
 export function searchInput(creators) {
   let searchValue = document.querySelector('.js-search-bar').value.trim().toLowerCase();
   let found = false;
+  let matchingCreator;
 
   Object.keys(creators).forEach(category => {
     creators[category].forEach(creator => {
       if(creator.name.toLowerCase().includes(searchValue)) {
-        let matchingCreator = document.getElementById(creator.id);
+        matchingCreator = document.getElementById(creator.id);
 
         if (matchingCreator) {
           matchingCreator.scrollIntoView({
             behavior: 'smooth',
             block: 'center'
-          });
-
-          matchingCreator.classList.add('highlight');
-          setTimeout(() => {
-            matchingCreator.classList.remove('highlight');
-          }, 2000);
+          });    
         }
         found = true;
       }
     });
   });;
+  if (found && matchingCreator) {
+    let count = 0;
+
+    const interval = setInterval(() => {
+      matchingCreator.classList.toggle('highlight');
+      count++;
+
+      if (count === 8) {
+        clearInterval(interval);
+        matchingCreator.classList.remove('highlight');
+      }
+    }, 300);
+  }
   if (!found) {
     notyf.error('No creator found');
   }
