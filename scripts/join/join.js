52365c08c3b6780dbf94d153287cf2659dc6bfb6
Peter Migichi <petermigichi47@gmail.com>
2026-07-03 17:38:24 +0300

    .

--- scripts/join/join.js
+++ scripts/join/join.js
@@ -1,5 +1,16 @@
 document.querySelector('.js-lets-go-btn')
 .addEventListener('click', () => {
   window.location.href = '/register.html';
+  getDetails();
 })
 
+// to be modified
+function getDetails() {
+  const name = document.querySelector('.js-name').value;
+  const admission = document.querySelector('.js-admission').value;
+  const year = document.querySelector('.js-year').value;
+  const platform = document.querySelector('.js-platform').value;
+  const followers = document.querySelector('.js-followers').value;
+  const handle = document.querySelector('.js-handle').value;
+  console.log(name, admission, year, platform, followers, handle);
+}
