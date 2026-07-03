4707f70415c1ad8519d51b8b954112ac62335633
Peter Migichi <petermigichi47@gmail.com>
2026-07-03 17:13:24 +0300

    designed the creator registration form

--- scripts/join/register.js
+++ scripts/join/register.js
@@ -2,6 +2,7 @@ document.querySelectorAll('.js-category-card')
 .forEach((card) => {
   const { category } = card.dataset;
   card.addEventListener('click', () => {
+    // to be removed
     console.log(`Chose ${category}`);
   })
 })
