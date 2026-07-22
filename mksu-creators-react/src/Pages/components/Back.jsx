505eb9ce94f99dc5a7c00a425b3b92973214cbc6
Peter Migichi <petermigichi47@gmail.com>
2026-07-22 07:23:20 +0300

    .

--- mksu-creators-react/src/Pages/components/Back.jsx
+++ mksu-creators-react/src/Pages/components/Back.jsx
@@ -1,5 +1,6 @@
-import { useNavigate } from "react-router-dom";
 
+import { useNavigate } from "react-router-dom";
+import "./Back.css"
 
 export default function Back(props) {
   const navigate = useNavigate();
