dcb9291e66f22d41073c27a1ca5f2824479aa055
Peter Migichi <petermigichi47@gmail.com>
2026-07-23 15:14:11 +0300

    .

--- mksu-creators-react/src/Pages/components/CreatorCard.jsx
+++ mksu-creators-react/src/Pages/components/CreatorCard.jsx
@@ -3,15 +3,18 @@ import "./CreatorCard.css";
 export function CreatorCard(props) {
   return (
     <div className="creator-card">
-      <img className={!props.image && "default-image"} src={props.image || "images/default.png"} />
+      <img
+        className={!props.image && "default-image"}
+        src={props.image || "images/default.png"}
+      />
       <div className="creator-card-text">
         <div>
           <p className="name">{props.name}</p>
         </div>
         <div className="creator-card-description">
           <p>{props.followers}k followers</p>
-          <small>{'\u25CF'}</small>
-          <p>Year {props.year || 'N/A'} </p>
+          <small>{"\u25CF"}</small>
+          <p>Year {props.year || "N/A"} </p>
         </div>
       </div>
     </div>
--- mksu-creators-react/src/Pages/components/Header.jsx
+++ mksu-creators-react/src/Pages/components/Header.jsx
@@ -11,6 +11,13 @@ export function Header() {
         />
         <h1 className="header-title">MKSU CREATORS AWARDS</h1>
       </div>
+      <div>
+        <div className="confirm-votes">
+          <div className="vote-count">0</div>
+          <p>votes</p>
+          <button>confirm</button>
+        </div>
+      </div>
       <hr className="header-hr" />
     </div>
   );
