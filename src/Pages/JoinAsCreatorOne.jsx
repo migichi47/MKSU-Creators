6da125bf25e3ef3e5a3174b702217c9afe112111
Peter Migichi <petermigichi47@gmail.com>
2026-07-31 09:48:25 +0300

    fetch data from join as creator form

--- src/Pages/JoinAsCreatorOne.jsx
+++ src/Pages/JoinAsCreatorOne.jsx
@@ -2,16 +2,37 @@ import { Header } from "./components/Header";
 import { useNavigate } from "react-router-dom";
 import Back from "./components/Back";
 
-
 import "./JoinAsCreatorCss/1.css";
 import "./JoinAsCreatorCss/media.css";
+import { useState } from "react";
 
 export function JoinAsCreatorOne() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+    admission: "",
+    phoneNumber: "",
+    year: "",
+    platform: "",
+    followers: "",
+  });
+
   const navigate = useNavigate();
+  function submitCreatorDetails() {
+    // navigate("/join-as-creator-two");
+    console.log(formData);
+  }
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <>
-      <Back navigate="/"/>
+      <Back navigate="/" />
       <Header />
 
       <div className="intro">
@@ -27,9 +48,20 @@ export function JoinAsCreatorOne() {
           <input
             className="join-tile-input js-name"
             type="text"
-            placeholder="Name"
-            name=""
-            id=""
+            placeholder="Full Name"
+            name="fullName"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="handle-section">
+          {"\u2022"}
+          <input
+            className="join-tile-input js-handle"
+            type="text"
+            placeholder="username eg. mutiso123"
+            name="username"
+            onChange={handleChange}
           />
         </div>
 
@@ -39,6 +71,8 @@ export function JoinAsCreatorOne() {
             className="join-tile-input js-admission"
             type="text"
             placeholder="Adm. eg. J17-0458-2025"
+            name="admission"
+            onChange={handleChange}
           />
         </div>
 
@@ -48,6 +82,8 @@ export function JoinAsCreatorOne() {
             className="join-tile-input js-contact"
             type="text"
             placeholder="Phone no."
+            name="phoneNumber"
+            onChange={handleChange}
           />
         </div>
 
@@ -56,8 +92,9 @@ export function JoinAsCreatorOne() {
           <select
             title="year"
             className="join-tile-select js-year"
-            name=""
-            id=""
+            onChange={handleChange}
+            name="year"
+            value={formData.year}
           >
             <option value="1">Year 1</option>
             <option value="2">Year 2</option>
@@ -74,8 +111,9 @@ export function JoinAsCreatorOne() {
           <select
             title="year"
             className="join-tile-select js-platform"
-            name=""
-            id=""
+            onChange={handleChange}
+            name="platform"
+            value={formData.platform}
           >
             <option value="tiktok">TikTok</option>
             <option value="instagram">Instagram</option>
@@ -88,20 +126,14 @@ export function JoinAsCreatorOne() {
             className="join-tile-input js-followers"
             type="text"
             placeholder="Followers eg. 6700"
+            name="followers"
+            onChange={handleChange}
           />
         </div>
 
-        <div className="handle-section">
-          {"\u2022"}
-          <input
-            className="join-tile-input js-handle"
-            type="text"
-            placeholder="handle   eg. @omondi"
-            name=""
-            id=""
-          />
-        </div>
-        <button className="lets-go-btn" onClick={() => navigate("/join-as-creator-two")}>Let's Go</button>
+        <button className="lets-go-btn" onClick={() => submitCreatorDetails()}>
+          Let's Go
+        </button>
       </div>
     </>
   );
