4b0763608d5d21edc8099a4a05e2d9b95df8b4ff
Peter Migichi <petermigichi47@gmail.com>
2026-07-31 22:50:56 +0300

    load image from server pc,, and imageUrl from db, all these meet in backend to server frontend a complete user

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,13 @@ app.listen(PORT, () => {
 app.get("/creators", async (request, response) => {
   try {
     const creators = await Creator.find();
-    response.status(200).json(creators);
+
+    const formattedCreators = creators.map(creator => ({
+      ...creator._doc,
+      image: `http://localhost:3000/uploads/${creator.image}`
+    }))
+
+    response.status(200).json(formattedCreators);
   } catch (err) {
     response.status(500).json({ error: err.message });
   }
