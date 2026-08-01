d74cda16cacff0ada166e01d8b2cc50cb303230c
Peter Migichi <petermigichi47@gmail.com>
2026-08-01 13:57:55 +0300

    .

--- backend/mongoose/schemas.js
+++ backend/mongoose/schemas.js
@@ -5,7 +5,7 @@ const creatorSchema = new mongoose.Schema({
   followers: { type: String, required: true },
   category: { type: String, required: true },
   platform: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   admission: { type: String, required: true },
   fullName: { type: String, required: true },
   year: { type: String, required: true },
