56defac1d63b5872eedb653cf9cd15b8e7093792
Peter Migichi <petermigichi47@gmail.com>
2026-08-10 22:35:12 +0300

    fully customize homepage lightmode

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -7,7 +7,7 @@ export function HomePage() {
   return (
     <div className="text-tertiary">
       <Header />
-      <div className="flex flex-col relative top-22 min-h-screen border text-2xl text-center justify-center gap-20">
+      <div className="flex flex-col relative top-22 min-h-screen text-2xl text-center justify-center gap-20">
         <div className="flex flex-col gap-5">
           <p className="text-3xl font-bold">
             Machakos University creators awards are here!
@@ -20,14 +20,17 @@ export function HomePage() {
           </p>
         </div>
 
-        <div> 
-          <button className="border border-primary/50 text-tertiary px-6 py-2 rounded-full hover:bg-primary/50 transition-colors" onClick={() => navigate("voting")}>
+        <div>
+          <button
+            className="border border-primary/50 text-tertiary px-6 py-2 rounded-full hover:bg-primary/50 transition-colors"
+            onClick={() => navigate("voting")}
+          >
             Vote Now
           </button>
           <div className="mt-4 flex gap-4 mx-auto w-fit">
             <span className="text-lg">Are you a Creator ?</span>
             <span
-              className="text-lg hover:text-primary hover:underline cursor-pointer"
+              className="text-lg hover:text-primary hover:underline cursor-pointer transition-colors"
               onClick={() => {
                 navigate("join-as-creator-one");
               }}
@@ -38,27 +41,21 @@ export function HomePage() {
         </div>
       </div>
 
-      <div className="footer-section">
-        <div className="footer">
-          <div className="collaborator-logo">
-            Sponsored by:
-            <img src="images/Migichi.png" alt="" />
-          </div>
-          <div className="collaborator-info"></div>
-          <div className="footer-info">
-            <a href="">Send Queries</a>
-            <a href="">Contact Sponsors</a>
-          </div>
+      {/* footer-section */}
+      <div className="border-t border-tertiary/20 text-center pt-4 pb-8 flex flex-col gap-2">
+        <h1 className="text-2xl font-bold text-primary">MKSU grammys</h1>
+        <div className="text-secondary flex gap-2 mx-auto ">
+          <a href="#" className="">
+            Privacy Policy
+          </a>
+          <a href="#" className="">
+            Terms of Excellence
+          </a>
+          <a href="#" className="">
+            Sponsors
+          </a>
         </div>
-        <hr className="footer-hr" />
-        <a href="#top">&#8593 Back to top</a>
-        <br />
-        <footer className="footer-details">
-          <p>&copy; 2024 MKSU Creators Awards. All rights reserved.</p>
-        </footer>
-        <button className="admin-btn" onClick={() => navigate("/admin")}>
-          Admin
-        </button>
+        <span className="text-primary/90">&copy;2026 MKSU Creators Awards. All rights reserved</span>
       </div>
     </div>
   );
