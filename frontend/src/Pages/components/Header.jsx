d69ba5b56bec425f3a178b43f7ec112a9cd98eb4
Peter Migichi <petermigichi47@gmail.com>
2026-08-10 15:50:01 +0300

    create a desktop header in tailwind

--- frontend/src/Pages/components/Header.jsx
+++ frontend/src/Pages/components/Header.jsx
@@ -1,22 +1,37 @@
-import { useNavigate } from "react-router-dom";
-import "./Header.css";
 
-export function Header(props) {
-  const { description } = props;
-  const navigate = useNavigate();
+import { FaRegMoon } from "react-icons/fa";
+
+export function Header() {
   return (
-    <div className="header">
-      <div className="header-section js-header-section">
-        <img
-          className="header-image"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLN-ym3nfLTARYaYOaAr5gEL0pHcpAHGqYqiFQcsF6ZL2OLgmt-hGmx-k&s=10"
-          alt=""
-          onClick={() => navigate("/")}
-        />
-        <h1 className="header-title">MKSU CREATORS AWARDS</h1>
-        <h6 className="header-description">{description}</h6>
+    <div className="fixed top-0  min-h-22 w-full flex z-10 items-center justify-between backdrop-blur-xs border-b border-neutral/20 px-6">
+      <div className="text-2xl cursor-pointer">
+        <span className="text-tertiary">MKSU</span>
+        <span className="text-primary">grammys</span>
       </div>
-      <hr className="header-hr" />
+
+      <div className="flex space-x-8 items-center">
+        <div className="flex space-x-3 text-tertiary eac">
+          <a href="" className="hover:text-secondary">
+            Dancers
+          </a>
+          <a href="" className="hover:text-secondary">
+            Vloggers
+          </a>
+          <a href="" className="hover:text-secondary">
+            Influencers
+          </a>
+          <a href="" className="hover:text-secondary">
+            Comedians
+          </a>
+          <a href="" className="hover:text-secondary">
+            Musicians
+          </a>
+        </div>
+
+        <div>
+          <FaRegMoon />
+        </div>
+      </div>
     </div>
   );
 }
