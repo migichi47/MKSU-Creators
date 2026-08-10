2c51a12642641cf4990ced2121f60f0e6712820e
Peter Migichi <petermigichi47@gmail.com>
2026-08-10 22:13:08 +0300

    complete hero style

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -7,29 +7,34 @@ export function HomePage() {
   return (
     <div className="text-tertiary">
       <Header />
-      <div className="flex flex-col relative top-22 min-h-screen border text-2xl text-center justify-center">
-        <p className="text-3xl font-bold">
-          Machakos University creators awards are here!
-        </p>
-        <p className="text-7xl font-extrabold text-primary">Cast Your Vote</p>
-        <button className="" onClick={() => navigate("voting")}>
-          Vote Now
-        </button>
-        <p>
-          Celebrate the pinnacle of digital excellence. Honor the creators who
-          redefine storytelling, performance, and community in the modern era.
-          Your voice decides the elite.
-        </p>
-        <div className="">
-          <span className="">Are you a Creator ?</span>
-          <button
-            className=""
-            onClick={() => {
-              navigate("join-as-creator-one");
-            }}
-          >
-            Join now
+      <div className="flex flex-col relative top-22 min-h-screen border text-2xl text-center justify-center gap-20">
+        <div className="flex flex-col gap-5">
+          <p className="text-3xl font-bold">
+            Machakos University creators awards are here!
+          </p>
+          <p className="text-7xl font-extrabold text-primary">Cast Your Vote</p>
+          <p className="text-lg leading-5 max-w-100 mx-auto">
+            Celebrate the pinnacle of digital excellence. Honor the creators who
+            redefine storytelling, performance, and community in the modern era.
+            Your voice decides the elite.
+          </p>
+        </div>
+
+        <div> 
+          <button className="border border-primary/50 text-tertiary px-6 py-2 rounded-full hover:bg-primary/50 transition-colors" onClick={() => navigate("voting")}>
+            Vote Now
           </button>
+          <div className="mt-4 flex gap-4 mx-auto w-fit">
+            <span className="text-lg">Are you a Creator ?</span>
+            <span
+              className="text-lg hover:text-primary hover:underline cursor-pointer"
+              onClick={() => {
+                navigate("join-as-creator-one");
+              }}
+            >
+              Join now
+            </span>
+          </div>
         </div>
       </div>
 
