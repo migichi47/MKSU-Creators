326810cda5639a082f14fcd336a04792e001a7d4
Peter Migichi <petermigichi47@gmail.com>
2026-08-11 16:11:28 +0300

    .

--- frontend/src/Pages/YourVotes.jsx
+++ frontend/src/Pages/YourVotes.jsx
@@ -1,7 +1,12 @@
+import { toast } from "react-toastify";
+
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
 export function YourVotes({ selectedCreators, setSelectedCreators }) {
+  function clearCreators() {
+    setSelectedCreators([]);
+  }
   return (
     <div>
       <Header />
@@ -48,8 +53,12 @@ export function YourVotes({ selectedCreators, setSelectedCreators }) {
 
       {/* action buttons */}
       <div className="mx-auto w-fit mt-10 flex gap-6 [&>a]:px-6 [&>a]:py-2 [&>a]:rounded-full [&>a]:cursor-pointer [&>a]:hover:scale-110 [&>a]:transition-all duration-200 [&>a]:hover:shadow-lg">
-        <a className="bg-green-500/80">Confirm Votes</a>
-        <a className="bg-red-500/80">Clear All</a>
+        <a className="bg-green-500/80" onClick={() => toast("Votes confirmed")}>
+          Confirm Votes
+        </a>
+        <a className="bg-red-500/80" onClick={clearCreators}>
+          Clear All
+        </a>
       </div>
 
       <Footer />
