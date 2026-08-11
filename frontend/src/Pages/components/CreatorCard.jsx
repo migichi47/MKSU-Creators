fa4189bb2e5f882f6ba87bd44f1e8125ab1c99f5
Peter Migichi <petermigichi47@gmail.com>
2026-08-11 11:25:57 +0300

    .

--- frontend/src/Pages/components/CreatorCard.jsx
+++ frontend/src/Pages/components/CreatorCard.jsx
@@ -15,7 +15,7 @@ export function CreatorCard({ creator }) {
       <div className=" absolute top-0 rounded-[inherit] min-w-[inherit] h-[inherit] bg-linear-to-b from-neutral/0 to-tertiary/80" />
 
       <div className="absolute flex flex-col gap-4 bottom-3 px-6 rounded-b-2xl w-full text-neutral">
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-2">
           <span className="font-semibold text-4xl">{name}</span>
           <div className="flex gap-3 text-xs">
             <span>{followers}</span>
