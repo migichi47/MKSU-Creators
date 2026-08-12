8f1fe0775abe6d6b9d875af06aff21568176b17c
Peter Migichi <petermigichi47@gmail.com>
2026-08-12 16:18:49 +0300

    add amnimation

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -20,13 +20,13 @@ export function HomePage({
       {/* hero section */}
       <div className="flex flex-col relative top-22 min-h-screen text-2xl text-center justify-center gap-20">
         <div className="flex flex-col gap-5">
-          <p className="md:text-5xl xs:text-3xl text-2xl font-bold sm:max-w-100 md:max-w-200 max-w-70 mx-auto">
+          <p className="md:text-5xl xs:text-3xl text-2xl font-bold sm:max-w-100 md:max-w-200 max-w-70 mx-auto slide-from-top duration-1000">
             Machakos University creators awards are here!
           </p>
-          <p className=" font-extrabold text-primary text-6xl md:text-8xl">
+          <p className=" font-extrabold text-primary text-6xl md:text-8xl slide-from-top duration-1000 delay-200">
             Cast Your Vote
           </p>
-          <p className="text-sm sm:text-lg leading-5 max-w-100 mx-auto">
+          <p className="text-sm sm:text-lg leading-5 max-w-100 mx-auto slide-from-top duration-1000 delay-300">
             Celebrate the pinnacle of digital excellence. Honor the creators who
             redefine storytelling, performance, and community in the modern era.
             Your voice decides the elite.
@@ -35,7 +35,7 @@ export function HomePage({
 
         <div>
           <button
-            className="bg-amber-50/0 hover:bg-primary hover:text-neutral dark:hover:text-tertiary dark:hover:bg-neutral px-6 py-2 md:text-3xl md:px-10 md:py-4 rounded-full transition-colors"
+            className="bg-amber-50/0 hover:bg-primary hover:text-neutral dark:hover:text-tertiary dark:hover:bg-neutral px-6 py-2 md:text-3xl md:px-10 md:py-4 rounded-full transition-colors slide-from-top duration-1000 delay-400"
             onClick={() => {
               document.getElementById("voting-grid").scrollIntoView({
                 behavior: "smooth",
@@ -45,7 +45,7 @@ export function HomePage({
           >
             Vote Now
           </button>
-          <div className="mt-4 flex gap-4 mx-auto w-fit">
+          <div className="mt-4 flex gap-4 mx-auto w-fit ">
             <span className="text-lg">Are you a Creator ?</span>
             <span
               className="text-lg hover:text-secondary underline cursor-pointer transition-colors"
