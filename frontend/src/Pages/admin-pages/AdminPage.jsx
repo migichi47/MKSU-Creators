7de912d9177df1f5e95dd14e4d57b0bb7ec2c7d6
Peter Migichi <petermigichi47@gmail.com>
2026-08-14 14:55:56 +0300

    add navigation links

--- frontend/src/Pages/admin-pages/AdminPage.jsx
+++ frontend/src/Pages/admin-pages/AdminPage.jsx
@@ -8,6 +8,11 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FaRegMoon } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CiLight } from "react-icons/ci";
+import { LiaAwardSolid } from "react-icons/lia";
 
 export function AdminPage() {
   const data = [
@@ -16,12 +21,133 @@ export function AdminPage() {
     { name: "opiri", voters: 50 },
     { name: "daisy", voters: 20 },
   ];
-
+  const [showMenu, setShowMenu] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
   const navigate = useNavigate();
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
 
   return (
     <>
-      <div className="">
+      {/* header */}
+      <div className="fixed top-0  min-h-22 w-full flex z-10 items-center justify-between backdrop-blur-xs shadow-lg px-6 dark:bg-tertiary/50">
+        <a
+          className="cursor-pointer flex space-x-2 font-bold text-lg md:text-2xl "
+          href="#top"
+        >
+          <span className="text-tertiary dark:text-neutral">MKSU</span>
+          <span className="text-primary">
+            Gr
+            <LiaAwardSolid className="inline w-5" />
+            mmys
+          </span>
+        </a>
+
+        <div className="flex space-x-8 items-center">
+          <div className="hidden space-x-3 md:flex [&>a]:cursor-pointer">
+            <a
+              onClick={() => {
+                navigate("/admin");
+              }}
+              className="hover:text-secondary"
+            >
+              Dashboard
+            </a>
+            <a
+              onClick={() => {
+                navigate("/admin/creators");
+              }}
+              className="hover:text-secondary"
+            >
+              View Creators
+            </a>
+            <a
+              onClick={() => navigate("admin/add-creator")}
+              className="hover:text-secondary"
+            >
+              Add Creator
+            </a>
+            <a
+              onClick={() => {
+                navigate("/admin/analytics");
+              }}
+              className="hover:text-secondary"
+            >
+              Analytics
+            </a>
+          </div>
+
+          <div>
+            {/* hamburger menu */}
+            <GiHamburgerMenu
+              className="md:hidden hover:text-secondary w-7 h-7"
+              onClick={() => setShowMenu((prev) => !prev)}
+            />
+          </div>
+
+          <div className="w-fit">
+            {darkMode ? (
+              <CiLight
+                className="hover:text-secondary w-7 h-7 md:w-5 md:h-5"
+                onClick={toggleDarkMode}
+              />
+            ) : (
+              <FaRegMoon
+                className="hover:text-secondary w-5 h-5 md:w-5 md:h-5"
+                onClick={toggleDarkMode}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      {
+        /* separate menu */
+        showMenu && (
+          <div className="fixed bg-neutral/80 md:hidden dark:bg-tertiary/30 dark:border dark:border-neutral/20 text-xl gap-2 right-10 top-24  dark:backdrop-blur-sm z-10 flex flex-col border border-tertiary/20 shadow-lg px-8 py-4 rounded-2xl slide-from-top duration-400">
+            <a
+              href="#dancer"
+              className="hover:text-secondary"
+              onClick={() => setShowMenu(false)}
+            >
+              Dancers
+            </a>
+            <a
+              href="#vlogger"
+              className="hover:text-secondary"
+              onClick={() => setShowMenu(false)}
+            >
+              Vloggers
+            </a>
+            <a
+              href="#influencer"
+              className="hover:text-secondary"
+              onClick={() => setShowMenu(false)}
+            >
+              Influencers
+            </a>
+            <a
+              href="#comedian"
+              className="hover:text-secondary"
+              onClick={() => setShowMenu(false)}
+            >
+              Comedians
+            </a>
+            <a
+              href="#musician"
+              className="hover:text-secondary"
+              onClick={() => setShowMenu(false)}
+            >
+              Musicians
+            </a>
+          </div>
+        )
+      }
+
+      <div className="bg-black/20 w-fit mx-auto">
         <div
           className=""
           onClick={() => {
@@ -52,10 +178,7 @@ export function AdminPage() {
       </div>
 
       {/* add a bar chart - summary analytics */}
-      <ResponsiveContainer
-        width="70%"
-        height={350}
-      >
+      <ResponsiveContainer width="70%" height={350}>
         <div className="">LeaderBoard</div>
         <BarChart height={300} data={data} className="">
           <CartesianGrid strokeDasharray="3 3" />
