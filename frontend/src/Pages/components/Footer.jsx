7764c1656e1f3287f78ae82682b513e0f55c2b81
Peter Migichi <petermigichi47@gmail.com>
2026-08-14 16:23:44 +0300

    add dev tools

--- frontend/src/Pages/components/Footer.jsx
+++ frontend/src/Pages/components/Footer.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export function Footer() {
+  const navigate = useNavigate();
   return (
     <div className="relative bottom-0 border-t border-tertiary/20 dark:border-neutral/20 text-center pt-6 pb-8 flex flex-col gap-4 mt-10 text-xs">
       <div className="text-secondary/60 dark:text-neutral/80 flex gap-8 mx-auto [&>a:hover]:underline [&>a:hover]:text-tertiary/60 dark:[&>a:hover]:text-secondary transition-colors">
@@ -15,6 +18,9 @@ export function Footer() {
       <span className="text-primary/90">
         &copy;2026 MKSU Creators Awards. All rights reserved
       </span>
+      <button onClick={() => navigate("/admin")} className="w-fit mx-auto p-2">
+        Dev tools
+      </button>
     </div>
   );
 }
