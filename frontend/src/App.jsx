e5a72769b8b4c907990133d4b0a0208521adf7de
Peter Migichi <petermigichi47@gmail.com>
2026-08-21 14:32:09 +0300

    cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,24 @@ import { CreatorsPage } from "./Pages/admin-pages/CreatorsPage";
 import { AdminAddCreator } from "./Pages/admin-pages/add-creator/AddCreator";
 import { YourVotes } from "./Pages/your-votes/YourVotes";
 import { ContextProvider } from "./context/ContextProvider";
+import { AdminContextProvider } from "./Pages/admin-pages/AdminContextProvider";
 
 export default function App() {
   return (
     <ContextProvider>
-      <div className="">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/your-votes" element={<YourVotes />} />
-          <Route path="admin" element={<AdminPage />} />
-          <Route path="admin/creators" element={<CreatorsPage />} />
-          <Route path="admin/add-creator" element={<AdminAddCreator />} />
-        </Routes>
-        <ToastContainer />
-      </div>
+      <AdminContextProvider>
+        <div className="">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/your-votes" element={<YourVotes />} />
+
+            <Route path="admin" element={<AdminPage />} />
+            <Route path="admin/creators" element={<CreatorsPage />} />
+            <Route path="admin/add-creator" element={<AdminAddCreator />} />
+          </Routes>
+          <ToastContainer />
+        </div>
+      </AdminContextProvider>
     </ContextProvider>
   );
 }
--- frontend/src/Pages/admin-pages/add-creator/AddCreator.jsx
+++ frontend/src/Pages/admin-pages/add-creator/AddCreator.jsx
@@ -1,126 +1,22 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-import { useState } from "react";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+
 import { IoArrowBack } from "react-icons/io5";
 import { AdminHeader } from "../components/AdminHeader";
 import { AddCreatorForm } from "./AddCreatorForm";
+import { useContext } from "react";
+import { AddCreatorContext } from "../AdminContextProvider";
+import { useNavigate } from "react-router-dom";
 
 export function AdminAddCreator() {
-  const [formData, setFormData] = useState({
-    fullName: "",
-    username: "",
-    admission: "",
-    phoneNumber: "",
-    year: "1",
-    platform: "tiktok",
-    followers: "",
-    category: "dancer",
-  });
-  const [upload, setUpload] = useState();
+  const {
+    upload,
+    setUpload,
+    handleChange,
+    formData,
+    discardForm,
+    finishUpload,
+  } = useContext(AddCreatorContext);
   const navigate = useNavigate();
-
-  function handleChange(e) {
-    const { name, value, files } = e.target;
-
-    if (name === "image") {
-      setFormData((prev) => ({
-        ...prev,
-        image: files[0],
-      }));
-    } else {
-      setFormData((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    }
-  }
-
-  function discardForm() {
-    setFormData({
-      fullName: "",
-      username: "",
-      admission: "",
-      phoneNumber: "",
-      year: "1",
-      platform: "tiktok",
-      followers: "",
-      category: "dancer",
-    });
-    setUpload(null);
-    setTimeout(() => {
-      navigate("/admin");
-    }, 500);
-  }
-
-  async function finishUpload() {
-    // form validation
-    function validateForm() {
-      if (!formData.fullName.trim()) {
-        return "Full name is required";
-      }
-      if (!formData.username.trim()) {
-        return "Username is required";
-      }
-
-      if (!formData.admission.trim()) {
-        return "Admission number is required";
-      }
-
-      if (!formData.phoneNumber.trim()) {
-        return "Phone number is required";
-      }
-
-      if (isNaN(formData.phoneNumber)) {
-        return "phone number must be a number";
-      }
-
-      if (!formData.followers.trim()) {
-        return "followers required";
-      }
-
-      if (isNaN(formData.followers)) {
-        return "Followers must be a number";
-      }
-      if (!upload) {
-        return "upload your photo";
-      }
-    }
-
-    const error = validateForm();
-    if (error) {
-      alert(error);
-      return;
-    }
-
-    const form = new FormData();
-    Object.keys(formData).forEach((key) => {
-      form.append(key, formData[key]);
-    });
-    form.append("image", upload);
-
-    const response = await fetch(`${API_BASE_URL}/creators/add`, {
-      method: "POST",
-      body: form,
-    });
-    const data = await response.json();
-    console.log(data);
-
-    toast("Creator added successfully");
-    setTimeout(() => {
-      setFormData({
-        fullName: "",
-        username: "",
-        admission: "",
-        phoneNumber: "",
-        year: "1",
-        platform: "tiktok",
-        followers: "",
-        category: "",
-      });
-    }, 1000);
-  }
 
   return (
     <>
