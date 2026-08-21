ac330e172e5787900844f276b27eeb95061a5c58
Peter Migichi <petermigichi47@gmail.com>
2026-08-21 14:52:26 +0300

    .

--- frontend/src/Pages/admin-pages/add-creator/AddCreatorForm.jsx
+++ frontend/src/Pages/admin-pages/add-creator/AddCreatorForm.jsx
@@ -1,54 +1,17 @@
 import { useContext } from "react";
-import { RiImageAddFill } from "react-icons/ri";
-import { TiTickOutline, TiUserAddOutline } from "react-icons/ti";
+import { TiUserAddOutline } from "react-icons/ti";
 import { AddCreatorContext } from "../AdminContextProvider";
 import { contentCategories, ContentCategory } from "./ContentCategory";
+import { UploadImage } from "./UploadImage";
 
 export function AddCreatorForm() {
-  const {
-    upload,
-    setUpload,
-    handleChange,
-    formData,
-    discardForm,
-    finishUpload,
-  } = useContext(AddCreatorContext);
+  const { handleChange, formData, discardForm, finishUpload } =
+    useContext(AddCreatorContext);
 
   return (
     <div className="border border-tertiary/30 dark:border-neutral/30 px-5 rounded-xl">
       <div className="space-y-4 border-b border-tertiary/30 dark:border-neutral/30 py-10">
-        <label
-          className="bg-secondary/20 hover:bg-secondary/15 transition-colors cursor-pointer flex items-center gap-5 px-8 py-4 border border-dashed border-tertiary/30 rounded-xl mx-auto"
-          for="ImageUpload"
-        >
-          <h2 className="border cursor-pointer border-secondary/50 rounded-xl min-w-20 h-20 flex items-center justify-center">
-            <RiImageAddFill className="text-2xl text-secondary" />
-          </h2>
-          <div className="space-y-2">
-            <h2 className="font-semibold text-xl">Upload Photo</h2>
-            <p className="text-xs flex flex-col md:flex-row gap-1">
-              {upload ? (
-                <span className="flex items-center gap-1">
-                  File uploaded{" "}
-                  <TiTickOutline className="text-lg fill-secondary" />
-                </span>
-              ) : (
-                "Choose an image"
-              )}
-              <span className="opacity-60">(recommended JPG & PNG)</span>
-            </p>
-            <input
-              id="ImageUpload"
-              type="file"
-              name="image"
-              accept="image/*"
-              onChange={(e) => {
-                setUpload(e.target.files[0]);
-              }}
-              hidden
-            />
-          </div>
-        </label>
+        <UploadImage />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 mx-auto gap-4">
           <div className="space-y-2">
---
+++ frontend/src/Pages/admin-pages/add-creator/UploadImage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { RiImageAddFill } from "react-icons/ri";
+import { TiTickOutline } from "react-icons/ti";
+import { AddCreatorContext } from "../AdminContextProvider";
+
+export function UploadImage() {
+  const { upload, setUpload } = useContext(AddCreatorContext);
+
+  return (
+    <label
+      className="bg-secondary/20 hover:bg-secondary/15 transition-colors cursor-pointer flex items-center gap-5 px-8 py-4 border border-dashed border-tertiary/30 rounded-xl mx-auto"
+      for="ImageUpload"
+    >
+      <h2 className="border cursor-pointer border-secondary/50 rounded-xl min-w-20 h-20 flex items-center justify-center">
+        <RiImageAddFill className="text-2xl text-secondary" />
+      </h2>
+      <div className="space-y-2">
+        <h2 className="font-semibold text-xl">Upload Photo</h2>
+        <p className="text-xs flex flex-col md:flex-row gap-1">
+          {upload ? (
+            <span className="flex items-center gap-1">
+              File uploaded <TiTickOutline className="text-lg fill-secondary" />
+            </span>
+          ) : (
+            "Choose an image"
+          )}
+          <span className="opacity-60">(recommended JPG & PNG)</span>
+        </p>
+        <input
+          id="ImageUpload"
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={(e) => {
+            setUpload(e.target.files[0]);
+          }}
+          hidden
+        />
+      </div>
+    </label>
+  );
+}
