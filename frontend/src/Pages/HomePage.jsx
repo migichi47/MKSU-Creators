5049a63e9e292c99b12ad75438eb0b5ad8fd20c9
Peter Migichi <petermigichi47@gmail.com>
2026-08-21 13:17:36 +0300

    style the modal

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -103,7 +103,20 @@ export function HomePage() {
             setRenderModal(false);
           }}
         >
-          <h2>Modal rendered</h2>
+          <div className="">
+            <h2 className="font-semibold text-lg">
+              You voted in all categories
+            </h2>
+            <p className="text-sm text-zinc-800">Confirm your votes</p>
+            <div>
+              <button
+                className="text-xs px-2 py-1 mt-2"
+                onClick={() => navigate("/your-votes")}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
         </CompleteModal>
       )}
     </div>
--- frontend/src/Pages/portals/CompleteModal.jsx
+++ frontend/src/Pages/portals/CompleteModal.jsx
@@ -1,12 +1,18 @@
 import { createPortal } from "react-dom";
+import { MdCancel } from "react-icons/md";
 
 export function CompleteModal({ children, isOpen, onClose }) {
   if (!isOpen) return null;
 
   return createPortal(
-    <div className="fixed flex top-0 left-0 right-0 bottom-0 justify-center items-center bg-black/70">
-      <div>{children}</div>
-      <button onClick={onClose}>close</button>
+    <div className="fixed flex top-0 left-0 right-0 bottom-0 text-black text-center justify-center items-center bg-black/70">
+      <div className="flex relative flex-col bg-white rounded-2xl px-5 pt-8 pb-5">
+        <MdCancel
+          className="absolute right-5 top-3 text-xl"
+          onClick={onClose}
+        />
+        <div className="">{children}</div>
+      </div>
     </div>,
     document.body,
   );
