469be2330684bf840ab6d615fac3834f2e6eabda
Peter Migichi <petermigichi47@gmail.com>
2026-08-23 11:56:52 +0300

    install react Typed and use it in hero

--- frontend/src/Pages/home-page/Hero.jsx
+++ frontend/src/Pages/home-page/Hero.jsx
@@ -1,21 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { ReactTyped } from "react-typed";
 
 export function Hero() {
   const navigate = useNavigate();
-  
+
   return (
     <div className="flex flex-col relative top-22 min-h-screen text-2xl text-center justify-center gap-20">
       <div className="flex flex-col gap-5">
-        <p className="md:text-5xl xs:text-3xl text-2xl font-bold sm:max-w-100 md:max-w-200 max-w-70 mx-auto slide-from-top duration-1000">
+        <p className="text-2xl font-semibold sm:max-w-100 md:max-w-200 max-w-70 mx-auto slide-from-top duration-1000">
           Machakos University creators awards are here!
         </p>
-        <p className=" font-extrabold text-primary text-6xl md:text-8xl slide-from-top duration-1000 delay-200">
-          Cast Your Vote
-        </p>
+        <div className="flex w-fit mx-auto gap-2 font-bold text-primary text-2xl md:text-4xl lg:text-6xl slide-from-top duration-1000 delay-200">
+          <span>Vote for your favorite</span>
+          <span>
+            <ReactTyped
+              strings={[
+                "dancer",
+                "influencer",
+                "vlogger",
+                "musician",
+                "comedian",
+              ]}
+              typeSpeed={50}
+              backSpeed={30}
+              loop
+              className="text-secondary"
+            />
+          </span>
+        </div>
         <p className="text-xs sm:text-lg leading-5 max-w-100 mx-auto slide-from-top duration-1000 delay-300">
-          Celebrate the pinnacle of digital excellence. Honor the creators who
-          redefine storytelling, performance, and community in the modern era.
-          Your voice decides the elite.
+          Cast your vote now
         </p>
       </div>
 
