39cda22891c810df6436c4ab0514a4309e10ee4c
Peter Migichi <petermigichi47@gmail.com>
2026-08-23 10:46:09 +0300

    create a darkmode for admin login

--- frontend/src/Pages/admin-pages/auth/Login.jsx
+++ frontend/src/Pages/admin-pages/auth/Login.jsx
@@ -7,9 +7,17 @@ export function AdminLogin() {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
 
+  function submitForm(e) {
+    e.preventDefault();
+    navigate("/admin");
+  }
+
   return (
     <div className="flex items-center h-screen">
-      <form className="shadow-[2px_2px_10px_2px_rgba(0,0,0,0.2)] flex flex-col w-fit mx-auto items-center gap-3 p-8 rounded-sm">
+      <form
+        className="shadow-[2px_2px_10px_2px_rgba(0,0,0,0.2)] dark:shadow-[1px_1px_5px_2px_rgba(255,255,255,0.2)] dark:border dark:border-neutral/30 dark:bg-black/50 flex flex-col w-fit mx-auto items-center gap-5 p-8 rounded-sm py-15"
+        onSubmit={submitForm}
+      >
         <div
           className="cursor-pointer flex space-x-1 font-semibold text-sm"
           onClick={() => navigate("/")}
@@ -22,19 +30,19 @@ export function AdminLogin() {
           </span>
         </div>
         <h1 className="font-bold text-xl ">Welcome</h1>
-        <p className="text-zinc-500 text-xs">
+        <p className="text-zinc-500 dark:text-zinc-300 text-xs">
           Log in to continue to Admin Page
         </p>
         <input
           type="text"
           placeholder="email"
-          className="border border-zinc-300 p-2 w-70 outline-secondary/70"
+          className="border border-zinc-300 p-2 w-70 outline-secondary/70 rounded-sm dark:border-zinc-500"
         />
         <div className="relative">
           <input
             type={showPassword ? "text" : "password"}
             placeholder="password"
-            className="border border-zinc-300 p-2 w-70 outline-secondary/70"
+            className="border border-zinc-300 p-2 w-70 outline-secondary/70 rounded-sm dark:border-zinc-500"
           />
           <div
             className="absolute top-[30%] right-4 text-zinc-500 cursor-pointer"
@@ -44,22 +52,18 @@ export function AdminLogin() {
           </div>
         </div>
         <div className="flex justify-start w-full">
-          <span className="text-secondary font-semibold text-xs cursor-pointer hover:text-secondary/80 hover:underline transition-colors">
+          <span className="text-secondary dark:text-primary font-semibold text-xs cursor-pointer dark:hover:text-primary/80 hover:text-secondary/80 hover:underline transition-colors">
             Forgot Password?
           </span>
         </div>
         <button
           type="submit"
-          className="w-full py-2 border-0 bg-tertiary text-white group hover:bg-tertiary/90"
+          className="w-full py-2 border-0 bg-tertiary dark:bg-neutral dark:text-tertiary text-white dark:hover:bg-neutral/90 group hover:bg-tertiary/90"
         >
-          <span className="text-[15px] group-hover:text-[16px] duration-100">Continue</span>
-        </button>
-        <div className="flex w-full justify-start text-sm items-center gap-2">
-          <span className="font-light text-xs">Don't have an account?</span>
-          <span className="font-semibold text-secondary cursor-pointer hover:text-secondary/80 hover:underline transition-colors">
-            Sign up
+          <span className="text-[15px] group-hover:text-[16px] duration-100">
+            Continue
           </span>
-        </div>
+        </button>
       </form>
     </div>
   );
